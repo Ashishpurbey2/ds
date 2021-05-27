0b143bc079a308668df91095b1b8a6ca34b3f00f
Ashish Kumar <ashishpurbey900@gamil.com>
2021-05-27 11:42:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='body1'>
+      <section
+        className='section1'
+        style={{
+          width: '100%',
+          height: '41px',
+          backgroundColor: '#E4E4E4',
+        }}
+        className='top1'
+      >
+        <img src='./images/img2.png'></img>
+        <p>File</p>
+        <p>Edit</p>
+        <p>View</p>
+        <p>Window</p>
+        <p>Help</p>
+      </section>
+      <section
+        className='section2'
+        style={{
+          width: '100%',
+          height: '41px',
+          backgroundColor: '#F5F5F5',
+        }}
+        className='top2'
+      >
+        <img src='./images/img3.png'></img>
+        <p>Layers</p>
+        <p>Library</p>
+        <p>Transform</p>
+        <img src='./images/img1.png'></img>
+        <img src='./images/img1.png'></img>
+        <img src='./images/img1.png'></img>
+        <img src='./images/img1.png'></img>
+        <div
+          style={{ width: '0px', height: '33px', border: '2px solid black' }}
+        ></div>
+        <img src='./images/img1.png'></img>
+        <img src='./images/img1.png'></img>
+        <img src='./images/img1.png'></img>
+        <img src='./images/img1.png'></img>
+      </section>
+      <div
+        style={{
+          backgroundColor: '#E4E4E4',
+          width: '100%',
+          height: '100vh-61px',
+        }}
+        className='aside'
+      >
+        <div
+          style={{
+            width: '40px',
+            height: '100vh',
+            backgroundColor: '#F5F5F5',
+            marginTop: '1px',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+          <img src='./images/img5.png'></img>
+        </div>
+        <canvas
+          style={{ width: '70vw', height: '100vh', backgroundColor: '#E4E4E4' }}
+        ></canvas>
+        <div
+          style={{
+            width: '70px',
+            height: '100vh',
+            backgroundColor: '#F5F5F5',
+            marginTop: '1px',
+          }}
+        >
+          <div>
+            <img src='./images/img4.png'></img>
+            <p>Templates</p>
+          </div>
+          <div>
+            <img src='./images/img4.png'></img>
+            <p>Templates</p>
+          </div>
+          <div>
+            <img src='./images/img4.png'></img>
+            <p>Templates</p>
+          </div>
+          <div>
+            <img src='./images/img4.png'></img>
+            <p>Templates</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
