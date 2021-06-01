8079516a301eaf5501a5f5565431eace7c83f6a7
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2021-06-01 12:39:30 +0530

    rectangle + colour palette

--- src/ components.js/AsideLeft.js
+++ src/ components.js/AsideLeft.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { ColorPicker , useColor } from 'react-color-palette'
+import "react-color-palette/lib/css/styles.css";
 
-const AsideLeft = () => {
+
+const AsideLeft = (props) => {
+  const [color, setColor] = useColor("hex", "#121212");
+
+  useEffect(()=>{
+    props.onColorChange(color)
+  })
+
+  const [ visible, setVisible ] = useState(true)
   const [height, setHeight] = useState(window.innerHeight - 140)
-
   function myFunction() {
     setHeight(window.innerHeight - 140)
   }
@@ -19,15 +28,27 @@ const AsideLeft = () => {
       }}
       className='asideLeft'
     >
+      <div style={{position:"absolute",left:"3.1vw",bottom:"10vh",display:!visible ? "block":"none",zIndex:"10000000"}}>
+        <ColorPicker width={300} height={200} style={{position:"absolute", left:"100vh"}} 
+        color={color} 
+        onChange={setColor} hideHSV dark/>;
+      </div>
       <img style={{cursor:"pointer"}} src='./images/move@1X.png' alt="" ></img>
       <img style={{cursor:"pointer"}} src='./images/crop (1)@1X.png' alt=""></img>
       <img style={{cursor:"pointer"}} src='./images/line@1X.png' alt=""></img>
       <img style={{cursor:"pointer"}} src='./images/img5.png' alt=""></img>
-      <img style={{cursor:"pointer"}} src='./images/pen-tool@1X.png' alt=""></img>
-      <img style={{cursor:"pointer"}} src='./images/img3.png' alt=""></img>
+      <img style={{cursor:"pointer"}} src='./images/pen-tool@1X.png' alt="" ></img>
+      <img style={{cursor:"pointer"}} src='./images/img3.png' alt="" 
+        onClick={()=>{
+          props.onSelect(color)
+        }}
+      ></img>
       <img style={{cursor:"pointer"}} src='./images/text@1X.png' alt=""></img>
       <img style={{cursor:"pointer"}} src='./images/paintbrush@1X.png' alt=""></img>
-      <img style={{cursor:"pointer"}} src='./images/pallete@1X.png' alt=""></img>
+      <img style={{cursor:"pointer"}} src='./images/pallete@1X.png' onClick={()=>{
+        setVisible(!visible)
+        props.isVisible(visible)
+      }}alt="" />
       <img style={{cursor:"pointer"}} src='./images/paint-bucket@1X.png' alt=""></img>
       <img style={{cursor:"pointer"}} src='./images/zoom (1)@1X.png' alt=""></img>
       <img style={{cursor:"pointer"}} src='./images/img5.png' alt=""></img>
--- src/ components.js/AsideRight.js
+++ src/ components.js/AsideRight.js
@@ -18,45 +18,24 @@ const AsideRight = () => {
         marginTop: '85px',
         position: 'fixed',
         left: `${width + 'px'}`,
-        textAlign:"center"
       }}
       className='asideRight'
     >
-      <div style={{position:"relative", right:"12px"}}>
-      <div class="image-upload">
-        <label for="file-input" style={{cursor:"pointer"}}>
-            <img src="./images/img4.png"/>
-        </label>
-        <input id="file-input" type="file" accept="image/*" style={{ display:"none" }}/>
-        <p className='extraMargin'>Templates</p>
+      <div>
+        <img src='./images/img4.png'></img>
+        <p>Templates</p>
       </div>
+      <div>
+        <img src='./images/img4.png'></img>
+        <p className='extraMargin'>uploads</p>
       </div>
-      <div style={{position:"relative", right:"12px"}}>
-      <div class="image-upload" >
-        <label for="file-input" style={{cursor:"pointer"}}>
-            <img src="./images/img4.png"/>
-        </label>
-        <input id="file-input" type="file" accept="image/*" style={{ display:"none" }}/>
-        <p className='extraMargin'>Uploads</p>
-      </div>
-      </div>
-      <div style={{position:"relative", right:"12px"}}>
-      <div class="image-upload">
-        <label for="file-input" style={{cursor:"pointer"}}>
-            <img src="./images/img4.png"/>
-        </label>
-        <input id="file-input" type="file" accept="image/*" style={{ display:"none" }}/>
+      <div>
+        <img src='./images/img4.png'></img>
         <p className='extraMargin'>Pictures</p>
       </div>
-      </div>
-      <div style={{position:"relative", right:"12px"}}>
-      <div class="image-upload">
-        <label for="file-input" style={{cursor:"pointer"}}>
-            <img src="./images/Videos@1X.png"/>
-        </label>
-        <input id="file-input" type="file" accept="image/*" style={{ display:"none" }}/>
+      <div>
+        <img src='./images/Videos@1X.png'></img>
         <p className='extraMargin'>Videos</p>
-      </div>
       </div>
     </div>
   )
