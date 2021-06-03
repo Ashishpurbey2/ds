1a5f887507bdfad1231c99c80411926962a7deb1
Arkadipta-Chatterjee <64316512+Arkadipta-Chatterjee@users.noreply.github.com>
2021-06-03 23:13:11 +0530

    Update AsideRight.js

--- src/ components.js/AsideRight.js
+++ src/ components.js/AsideRight.js
@@ -1,66 +1,69 @@
-import React from 'react'
-import Modal from 'react-modal'
-import { useState, useEffect } from 'react'
-Modal.setAppElement('#root')
+import React from "react";
+import Modal from "react-modal";
+import { useState, useEffect } from "react";
+Modal.setAppElement("#root");
 const AsideRight = () => {
-  const [width, setWidth] = useState(window.innerWidth - 90)
+  const [width, setWidth] = useState(window.innerWidth - 90);
   const [modalisOpen, setmodalisOpen] = useState(false);
   function myFunction() {
-    setWidth(window.innerWidth - 90)
-    console.log(window.innerHeight)
+    setWidth(window.innerWidth - 90);
+    console.log(window.innerHeight);
   }
-  window.onresize = myFunction
+  window.onresize = myFunction;
   return (
     <div
       style={{
-        width: '80px',
-        height: '100vh',
-        backgroundColor: '#F5F5F5',
-        marginTop: '85px',
-        position: 'fixed',
-        left: `${width + 'px'}`,
+        width: "80px",
+        height: "100vh",
+        backgroundColor: "#F5F5F5",
+        marginTop: "85px",
+        position: "fixed",
+        left: `${width + "px"}`,
       }}
-      className='asideRight'
+      className="asideRight"
     >
       <div>
-        <img src='./images/img4.png' onClick={()=>setmodalisOpen(true)}></img>
+        <img src="./images/img4.png" onClick={() => setmodalisOpen(true)}></img>
         <p>Templates</p>
       </div>
       <div>
-        <img src='./images/img4.png' onClick={()=>setmodalisOpen(true)}></img>
-        <p className='extraMargin'>uploads</p>
+        <img src="./images/img4.png" onClick={() => setmodalisOpen(true)}></img>
+        <p className="extraMargin">uploads</p>
       </div>
       <div>
-        <img src='./images/img4.png' onClick={()=>setmodalisOpen(true)}></img>
-        <p className='extraMargin'>Pictures</p>
+        <img src="./images/img4.png" onClick={() => setmodalisOpen(true)}></img>
+        <p className="extraMargin">Pictures</p>
       </div>
       <div>
-        <img src='./images/Videos@1X.png' onClick={()=>setmodalisOpen(true)}></img>
-        <p className='extraMargin'>Videos</p>
+        <img
+          src="./images/Videos@1X.png"
+          onClick={() => setmodalisOpen(true)}
+        ></img>
+        <p className="extraMargin">Videos</p>
       </div>
-      <Modal isOpen={modalisOpen} onRequestClose={()=>setmodalisOpen(false)}
-      style={
-        {
+      <Modal
+        isOpen={modalisOpen}
+        onRequestClose={() => setmodalisOpen(false)}
+        style={{
           overlay: {
-            backgroundColor: 'white',
-            
+            backgroundColor: "white",
           },
           content: {
-            color: 'orange',
-            backgroundColor: 'grey',
-            top: '120px',
-            left: '300px',
-            right: '300px',
-            bottom: '100px'
-          }
-        }
-      }>
+            color: "orange",
+            backgroundColor: "light-grey",
+            top: "50px",
+            left: "1200px",
+            right: "100px",
+            bottom: "0px",
+          },
+        }}
+      >
         <h2>Upload File Here</h2>
         <button>Upload File</button>
-        <button onClick={()=>setmodalisOpen(false)}>Close</button>
+        <button onClick={() => setmodalisOpen(false)}>Close</button>
       </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default AsideRight
+export default AsideRight;
