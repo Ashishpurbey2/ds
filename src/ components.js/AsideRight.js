93615658e3304acb1408768373182367ccc1fcdb
Debraj Goswami <59834809+Debraj8301@users.noreply.github.com>
2021-06-03 16:33:20 +0530

    Update AsideRight.js

--- src/ components.js/AsideRight.js
+++ src/ components.js/AsideRight.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Modal from 'react-modal'
 import { useState, useEffect } from 'react'
-
+Modal.setAppElement('#root')
 const AsideRight = () => {
   const [width, setWidth] = useState(window.innerWidth - 90)
-
+  const [modalisOpen, setmodalisOpen] = useState(false);
   function myFunction() {
     setWidth(window.innerWidth - 90)
     console.log(window.innerHeight)
@@ -22,21 +23,42 @@ const AsideRight = () => {
       className='asideRight'
     >
       <div>
-        <img src='./images/img4.png'></img>
+        <img src='./images/img4.png' onClick={()=>setmodalisOpen(true)}></img>
         <p>Templates</p>
       </div>
       <div>
-        <img src='./images/img4.png'></img>
+        <img src='./images/img4.png' onClick={()=>setmodalisOpen(true)}></img>
         <p className='extraMargin'>uploads</p>
       </div>
       <div>
-        <img src='./images/img4.png'></img>
+        <img src='./images/img4.png' onClick={()=>setmodalisOpen(true)}></img>
         <p className='extraMargin'>Pictures</p>
       </div>
       <div>
-        <img src='./images/Videos@1X.png'></img>
+        <img src='./images/Videos@1X.png' onClick={()=>setmodalisOpen(true)}></img>
         <p className='extraMargin'>Videos</p>
       </div>
+      <Modal isOpen={modalisOpen} onRequestClose={()=>setmodalisOpen(false)}
+      style={
+        {
+          overlay: {
+            backgroundColor: 'white',
+            
+          },
+          content: {
+            color: 'orange',
+            backgroundColor: 'grey',
+            top: '120px',
+            left: '300px',
+            right: '300px',
+            bottom: '100px'
+          }
+        }
+      }>
+        <h2>Upload File Here</h2>
+        <button>Upload File</button>
+        <button onClick={()=>setmodalisOpen(false)}>Close</button>
+      </Modal>
     </div>
   )
 }
