bf71eef8ece8118834b6e435fe3c624eab5f42ca
Ashish Kumar <ashishpurbey900@gamil.com>
2021-06-07 00:14:17 +0530

    updating model to rhs

--- src/ components.js/AsideRight.js
+++ src/ components.js/AsideRight.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Modal from 'react-modal'
 import { useState, useEffect } from 'react'
-
+Modal.setAppElement('#root')
 const AsideRight = () => {
   const [width, setWidth] = useState(window.innerWidth - 90)
-
+  const [modalisOpen, setmodalisOpen] = useState(false)
+  const [width1, setWidth1] = useState(window.innerWidth - 90)
   function myFunction() {
     setWidth(window.innerWidth - 90)
-    console.log(window.innerHeight)
+    setWidth1(window.innerWidth - 90)
+    console.log('heeh', width1, width)
   }
   window.onresize = myFunction
   return (
@@ -18,24 +21,55 @@ const AsideRight = () => {
         marginTop: '85px',
         position: 'fixed',
         left: `${width + 'px'}`,
+        zIndex: '1000',
       }}
       className='asideRight'
     >
       <div>
-        <img src='./images/img4.png'></img>
+        <img src='./images/img4.png' onClick={() => setmodalisOpen(true)}></img>
         <p>Templates</p>
       </div>
       <div>
-        <img src='./images/img4.png'></img>
+        <img src='./images/img4.png' onClick={() => setmodalisOpen(true)}></img>
         <p className='extraMargin'>uploads</p>
       </div>
       <div>
-        <img src='./images/img4.png'></img>
+        <img src='./images/img4.png' onClick={() => setmodalisOpen(true)}></img>
         <p className='extraMargin'>Pictures</p>
       </div>
       <div>
-        <img src='./images/Videos@1X.png'></img>
+        <img
+          src='./images/Videos@1X.png'
+          onClick={() => setmodalisOpen(true)}
+        ></img>
         <p className='extraMargin'>Videos</p>
+      </div>
+      <div>
+        <Modal
+          isOpen={modalisOpen}
+          onRequestClose={() => setmodalisOpen(false)}
+          style={{
+            overlay: {
+              backgroundColor: 'transparent',
+              zIndex: '10000',
+            },
+            content: {
+              display: 'absolute',
+              position: 'fixed',
+              color: 'orange',
+              backgroundColor: 'rgb(245,245,245)',
+              left: { width1 },
+              bottom: '0px',
+              width: '300px',
+              marginRight: '35px',
+              top: '82px',
+            },
+          }}
+        >
+          <h2>Upload File Here</h2>
+          <button>Upload File</button>
+          <button onClick={() => setmodalisOpen(false)}>Close</button>
+        </Modal>
       </div>
     </div>
   )
