a967eb775980076cade05e99523b0a60a4042fed
Arkadipta-Chatterjee <64316512+Arkadipta-Chatterjee@users.noreply.github.com>
2021-06-19 02:48:36 +0530

    Updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,84 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+// Step 1 - set up express & mongoose
 
-const app = express();
-require("dotenv").config();
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-const port = process.env.PORT || 3000;
+var fs = require("fs");
+var path = require("path");
+require("dotenv/config");
+// Step 2 - connect to the database
 
-app.use(cors());
-app.use(express.json());
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log("connected");
+  }
+);
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
+// Step 4 - set up EJS
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Set EJS as templating engine
+
+// Step 5 - set up multer for storing uploaded files
+
+var multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
 });
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("mongo DB success");
+var upload = multer({ storage: storage });
+// Step 6 - load the mongoose model for Image
+
+var imgModel = require("./model");
+// Step 7 - the GET request handler that provides the HTML UI
+
+app.get("/", (req, res) => {
+  imgModel.find({}, (err, items) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("An error occurred", err);
+    } else {
+      res.render("../frontend/src/index.js", { items: items });
+    }
+  });
 });
+// Step 8 - the POST handler for processing the uploaded file
 
-const userRouter = require("./routes/user");
-app.use("/user", userRouter);
+app.post("/", upload.single("image"), (req, res, next) => {
+  var obj = {
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    img: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/uploads/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
+  imgModel.create(obj, (err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // item.save();
+      res.redirect("/");
+    }
+  });
+});
+// Step 9 - configure the server's port
+
+var port = process.env.PORT || "3000";
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log("Server listening on port", port);
 });
