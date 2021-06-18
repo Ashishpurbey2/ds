3f9bb2166ced905fcd1cf436f1f966522307009d
Arkadipta-Chatterjee <64316512+Arkadipta-Chatterjee@users.noreply.github.com>
2021-06-18 18:39:34 +0530

    Delete src directory

--- src/ components.js/konvaElements/Shapes.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-
-export default function Shapes() {
-    return (
-        <div>
-            <img style={{cursor:"pointer"}} src='./images/img3.png' alt=""></img>
-        </div>
-    )
-}
