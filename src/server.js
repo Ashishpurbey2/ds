cb886448247d34237505ea6ba3af2380b61ec131
Arkadipta-Chatterjee <64316512+Arkadipta-Chatterjee@users.noreply.github.com>
2021-06-26 14:00:25 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,24 +1,26 @@
-const express=require('express');
-const app=express();
-const cors=require('cors');
-const multer=require('multer');
-const upload=multer({dest:"./uploads/"})
-const fs=require('fs');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "./uploads/" });
+const fs = require("fs");
 
-const PORT=3000;
+const PORT = 3000;
 
-app.use(cors);
-app.use(express.static("./uploads"));
+// app.use(cors);
+// app.use(express.static("./uploads"));
 
-app.post("/uploadFile",upload.single("image"),(req,res)=>
-{
-  let fileType=req.file.mimetype.split("/")[1];
-  let newFileName=req.file.filename+"."+fileType;
+app.post("/uploadFile", upload.single("image"), (req, res) => {
+  let fileType = req.file.mimetype.split("/")[1];
+  let newFileName = req.file.filename + "." + fileType;
 
-  fs.rename('./uploads/${req.file.filename}','./uploads/{newFileName}',function(){
-    res.send("200");
-  })
-
+  fs.rename(
+    "./uploads/" + req.file.filename,
+    "./uploads/" + newFileName,
+    function () {
+      res.send("200");
+    }
+  );
 });
 
-app.listen(PORT,()=>console.log('App listening on Port ${PORT}'))
+app.listen(PORT, () => console.log("App listening on Port", PORT));
