d640792bd23dc8c3eb35d7e3d28962973beac65f
Arkadipta-Chatterjee <64316512+Arkadipta-Chatterjee@users.noreply.github.com>
2021-07-01 12:14:15 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "./uploads/" });
+const fs = require("fs");
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.static("./uploads"));
+
+app.post("/uploadFile", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  let fileType = req.file.mimetype.split("/")[1];
+  let newFileName = req.file.filename + "." + fileType;
+
+  fs.rename(
+    "./uploads/" + req.file.filename,
+    "./uploads/" + newFileName,
+    function () {
+      res.send("200");
+    }
+  );
+});
+
+app.listen(PORT, () => console.log("App listening on Port", PORT));
